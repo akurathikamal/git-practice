fa96056afe43d0e2dc586127f2cf0fff81092df7
kamal <kamalakurathi860@gmail.com>
2024-10-07 19:34:45 +0530

    Arraylist is implemented

---
+++ arraylist.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+class arraylist{
+    public static void main(String[] args){
+        ArrayList<Integer> list= new ArrayList<>(5);
+        
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        System.out.println(list);
+    }
+}
